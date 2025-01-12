824bf7e8d6393205a1665d7d5de8fa15828b26ca
alejandro <alejo.jaz224@gmail.com>
2025-01-12 12:20:32 -0500

    hola

--- PromQuim.java
+++ PromQuim.java
@@ -2,21 +2,25 @@
 import java.util.Scanner;
 public class PromQuim{
 	public static void main (String[] args){
-		float numUno,numDos,promNotas;
+		float numUno,numDos,numTres,promNotas;
 		Scanner entrada=new Scanner(System.in);
 		System.out.println("ingrese primera nota:");
 		numUno=entrada.nextFloat();
 		System.out.println("ingrese lasegunda nota:");
 		numDos=entrada.nextFloat();
-		promNotas=(numUno+numDos)/2;
+		System.out.println("ingrese la tercera nota:");
+		numTres=entrada.nextFloat();
+		promNotas=(numUno + numDos + numTres)/3;
 		
-		if(promNotas>=5){
-			System.out.println("su promedioes"+promNotas);
-			System.out.println("usted pasa el año directamente");
-		}
-		else if (promNotas<5){
+		if (promNotas>=0 && promNotas<=4){
+			System.out.println("su promedio es"+promNotas);
+			System.out.println("perdido el año");
+		} else if (promNotas >= 4.01 && promNotas <= 7){
 			System.out.println("su promdio es: "+promNotas);
 			System.out.println("usted esta suspenso");
+		} else if(promNotas >=7 && promNotas <=10){
+			System.out.println("supromedio es"+promNotas);
+			System.out.println("usted pasa el año");
 		}
 
 	}
