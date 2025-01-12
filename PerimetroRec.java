6436a9a0db1c4e6a23cf3352f9de2b6bb7f660a0
alejandro <alejo.jaz224@gmail.com>
2025-01-12 09:51:55 -0500

    mi primer git

---
+++ PerimetroRec.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PerimetroRec{
+	public static void main(String[] args){
+		double base,altura,perimetro;
+		Scanner entrada=new Scanner(System.in);
+		System.out.println("ingrese el lado");
+		base=entrada.nextDouble();
+		System.out.println("ingrse la altura");
+		altura=entrada.nextDouble();
+		perimetro= 2*(base+altura);
+		System.out.println("el perimetro es: "+ perimetro);
+	}
+}
